97487d45cdea274af57b39527658e29354db4920
Houdael <houdaelmouden@gmail.com>
2026-05-09 14:11:15 +0200

    feat: finalize stg_flights staging model

--- transformations/aviation_analytics/models/staging/stg_flights.sql
+++ transformations/aviation_analytics/models/staging/stg_flights.sql
@@ -1,44 +1,33 @@
-{{ config(schema='staging') }}
-
 with source as (
     select * from {{ source('opensky', 'flights') }}
 ),
 
-renamed as (
+staged as (
     select
         -- surrogate key
         {{ dbt_utils.generate_surrogate_key(['icao24', 'first_seen']) }} as flight_id,
 
         -- identifiers
-        cast(icao24 as varchar)                   as icao24,
-        trim(cast(callsign as varchar))           as callsign,
-        cast(flight_type as varchar)              as flight_type,
+        icao24,
+        trim(callsign)                            as callsign,
+        flight_type,
 
         -- resolved airport (the one this record is indexed against)
         case
-            when flight_type = 'arrival'   then cast(est_arrival_airport as varchar)
-            when flight_type = 'departure' then cast(est_departure_airport as varchar)
-        end                                       as airport,
+            when flight_type = 'arrival'   then est_arrival_airport
+            when flight_type = 'departure' then est_departure_airport
+        end                                       as airport_icao,
 
         -- airport references
-        cast(est_departure_airport as varchar)    as est_departure_airport,
-        cast(est_arrival_airport as varchar)      as est_arrival_airport,
+        est_departure_airport,
+        est_arrival_airport,
 
         -- timestamps
         to_timestamp(cast(first_seen as bigint))  as first_seen_at,
-        to_timestamp(cast(last_seen as bigint))   as last_seen_at,
-
-        -- distances (metres)
-        cast(est_departure_airport_horiz_distance as integer) as departure_horiz_distance_m,
-        cast(est_departure_airport_vert_distance as integer)  as departure_vert_distance_m,
-        cast(est_arrival_airport_horiz_distance as integer)   as arrival_horiz_distance_m,
-        cast(est_arrival_airport_vert_distance as integer)    as arrival_vert_distance_m,
-
-        -- candidate counts
-        cast(departure_airport_candidates_count as integer)   as departure_airport_candidates_count,
-        cast(arrival_airport_candidates_count as integer)     as arrival_airport_candidates_count
+        to_timestamp(cast(last_seen as bigint))   as last_seen_at
 
     from source
+    where first_seen is not null
 )
 
-select * from renamed
+select * from staged
